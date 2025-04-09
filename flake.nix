e25ab62187d94a93798e73c7c618313031e54b65
Jess Frazelle <github@jessfraz.com>
2025-04-08 23:45:06 -0700

    addition
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- flake.nix
+++ flake.nix
@@ -39,7 +39,31 @@
         })
         supportedSystems);
   in {
-    homeManagerModules.default = {pkgs, ...}: let
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      vimWithSubmodules = pkgs.stdenv.mkDerivation {
+        name = "vim-config";
+        src = ./.;
+
+        nativeBuildInputs = [pkgs.git];
+
+        buildPhase = ''
+          echo "Updating submodules..."
+          git submodule update --init --recursive
+        '';
+
+        installPhase = ''
+          mkdir -p $out
+          cp -r . $out/
+        '';
+      };
+    });
+
+    homeManagerModules = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+      vimSrc = self.packages.${system}.vimWithSubmodules;
+
       mkIfExists = path:
         if builtins.pathExists path
         then path
@@ -48,47 +72,48 @@
       alejandraPkg = alejandra.defaultPackage.${pkgs.system};
       rustAnalyzer = fenix.packages.${pkgs.system}.rust-analyzer;
     in {
-      home.packages = with pkgs; [
-        alejandraPkg
-        rustAnalyzer
-        go
-        gopls
-        typescript
-        typescript-language-server
-        ripgrep
-      ];
+      default = {...}: {
+        home.packages = with pkgs; [
+          alejandraPkg
+          rustAnalyzer
+          go
+          gopls
+          typescript
+          typescript-language-server
+          ripgrep
+        ];
 
-      programs.neovim = {
-        enable = true;
-        defaultEditor = true;
-        viAlias = true;
-        vimAlias = true;
+        programs.neovim = {
+          enable = true;
+          defaultEditor = true;
+          viAlias = true;
+          vimAlias = true;
+        };
+
+        home.file = {
+          ".vimrc".source = "${vimSrc}/vimrc";
+          ".config/nvim/init.vim".source = "${vimSrc}/vimrc";
+
+          ".vim/autoload".source = mkIfExists "${vimSrc}/autoload";
+          ".config/nvim/autoload".source = mkIfExists "${vimSrc}/autoload";
+
+          ".vim/bundle".source = mkIfExists "${vimSrc}/bundle";
+          ".config/nvim/bundle".source = mkIfExists "${vimSrc}/bundle";
+
+          ".vim/colors".source = mkIfExists "${vimSrc}/colors";
+          ".config/nvim/colors".source = mkIfExists "${vimSrc}/colors";
+
+          ".vim/indent".source = mkIfExists "${vimSrc}/indent";
+          ".config/nvim/indent".source = mkIfExists "${vimSrc}/indent";
+        };
       };
+    });
 
-      home.file = {
-        ".vimrc".source = ./vimrc;
-        ".config/nvim/init.vim".source = ./vimrc;
-
-        ".vim/autoload".source = mkIfExists ./autoload;
-        ".config/nvim/autoload".source = mkIfExists ./autoload;
-
-        ".vim/bundle".source = mkIfExists ./bundle;
-        ".config/nvim/bundle".source = mkIfExists ./bundle;
-
-        ".vim/colors".source = mkIfExists ./colors;
-        ".config/nvim/colors".source = mkIfExists ./colors;
-
-        ".vim/indent".source = mkIfExists ./indent;
-        ".config/nvim/indent".source = mkIfExists ./indent;
-      };
-    };
-
-    # Optional: Provide buildable homeConfigurations for testing
     homeConfigurations = forAllSystems (
       system:
         home-manager.lib.homeManagerConfiguration {
           pkgs = import nixpkgs {inherit system;};
-          modules = [self.homeManagerModules.default];
+          modules = [(self.homeManagerModules.${system}.default)];
         }
     );
   };
