c07f4fecf7a256c4a3164612ae1f49fbc8cc4f90
Jess Frazelle <github@jessfraz.com>
2025-04-08 23:25:25 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-# flake.nix
 {
   description = "Home Manager module for Jessfraz's Vim configuration (Neovim-ready)";
 
@@ -11,7 +10,6 @@
       inputs.nixpkgs.follows = "unstable";
     };
 
-    # rust, see https://github.com/nix-community/fenix#usage
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "unstable";
@@ -26,91 +24,70 @@
   outputs = {
     self,
     nixpkgs,
+    unstable,
     home-manager,
     fenix,
     alejandra,
     ...
   }: let
-    # List supported systems (Linux & Darwin on x86_64 and aarch64)
     supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
 
-    # Helper function to generate attributes for each system
     forAllSystems = f:
       builtins.listToAttrs (map (system: {
           name = system;
           value = f system;
         })
         supportedSystems);
-
-        # Create packages for each system
-    mkPackages = system: let
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-    unstablePkgs = import unstable {
-      inherit system;
-    };
-    fenixPkgs = fenix.packages.${system};
-    alejandraPkg = alejandra.defaultPackage.${system};
   in {
-    # Home Manager module defining the Vim/Neovim setup
-    homeManagerModules = {
-      # "default" module can be imported by Home Manager
-      default = {pkgs, ...}: let
-        mkIfExists = path:
-          if builtins.pathExists path
-          then path
-          else pkgs.emptyFile;
-      in {
-        # 1. Install necessary packages in the user environment
-        home.packages = with pkgs; [
-          # Neovim will be installed via programs.neovim below (so we omit pkgs.neovim here)
-          alejandraPkg # Alejandra formatter
-          fenixPkg.rust-analyzer # Rust analyzer via Fenix
-          go # Go compiler
-          gopls # Go language server
-          typescript # TypeScript (tsc)
-          typescript-language-server # TS LSP
-          ripgrep # ripgrep search tool
-        ];
+    homeManagerModules.default = {pkgs, ...}: let
+      mkIfExists = path:
+        if builtins.pathExists path
+        then path
+        else pkgs.emptyFile;
 
-        # 2. Neovim program configuration (Home Manager will install Neovim)
-        programs.neovim = {
-          enable = true;
-          defaultEditor = true; # Set $EDITOR to nvim
-          viAlias = true; # Provide `vi` as alias to nvim
-          vimAlias = true; # Provide `vim` alias to nvim
-          # (Neovim will read ~/.config/nvim/init.vim which we set below)
-        };
+      alejandraPkg = alejandra.defaultPackage.${pkgs.system};
+      rustAnalyzer = fenix.packages.${pkgs.system}.rust-analyzer;
+    in {
+      home.packages = with pkgs; [
+        alejandraPkg
+        rustAnalyzer
+        go
+        gopls
+        typescript
+        typescript-language-server
+        ripgrep
+      ];
 
-        # 3. Link vim configuration files and directories to home
-        home.file = {
-          # Main Vim config file -> ~/.vimrc and Neovim init.vim
-          ".vimrc".source = ./vimrc;
-          ".config/nvim/init.vim".source = ./vimrc;
+      programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        viAlias = true;
+        vimAlias = true;
+      };
 
-          # Vim plugin & config directories -> link into both ~/.vim and ~/.config/nvim
-          ".vim/autoload".source = mkIfExists ./autoload;
-          ".config/nvim/autoload".source = mkIfExists ./autoload;
+      home.file = {
+        ".vimrc".source = ./vimrc;
+        ".config/nvim/init.vim".source = ./vimrc;
 
-          ".vim/bundle".source = mkIfExists ./bundle;
-          ".config/nvim/bundle".source = mkIfExists ./bundle;
+        ".vim/autoload".source = mkIfExists ./autoload;
+        ".config/nvim/autoload".source = mkIfExists ./autoload;
 
-          ".vim/colors".source = mkIfExists ./colors;
-          ".config/nvim/colors".source = mkIfExists ./colors;
+        ".vim/bundle".source = mkIfExists ./bundle;
+        ".config/nvim/bundle".source = mkIfExists ./bundle;
 
-          ".vim/indent".source = mkIfExists ./indent;
-          ".config/nvim/indent".source = mkIfExists ./indent;
-        };
+        ".vim/colors".source = mkIfExists ./colors;
+        ".config/nvim/colors".source = mkIfExists ./colors;
+
+        ".vim/indent".source = mkIfExists ./indent;
+        ".config/nvim/indent".source = mkIfExists ./indent;
       };
     };
 
-    # Optionally, provide ready-to-use Home Manager configurations for all systems (not required if you import the module manually)
+    # Optional: Provide buildable homeConfigurations for testing
     homeConfigurations = forAllSystems (
       system:
         home-manager.lib.homeManagerConfiguration {
           pkgs = import nixpkgs {inherit system;};
-          # (If using NixOS or nix-darwin with Home Manager, provide proper homeDirectory and username)
           modules = [self.homeManagerModules.default];
         }
     );
