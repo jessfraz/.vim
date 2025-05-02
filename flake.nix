4825bfa27b5e5ec1f01fe7f1da6ec0ec1d27e893
Jess Frazelle <github@jessfraz.com>
2025-05-02 14:10:34 -0700

    updates
    Signed-off-by: Jess Frazelle <github@jessfraz.com>

--- flake.nix
+++ flake.nix
@@ -58,7 +58,6 @@
 
       alejandraPkg = alejandra.packages.${pkgs.system}.default;
       rustAnalyzer = fenix.packages.${pkgs.system}.rust-analyzer;
-      rustFmt = fenix.packages.${pkgs.system}.minimal.toolchain.rustfmt;
       kclLsp = modeling-app.packages.${pkgs.system}.kcl-language-server;
     in {
       home.packages = with pkgs; [
@@ -77,7 +76,6 @@
         pyright
         ripgrep
         rustAnalyzer
-        rustFmt
         ruff
         stylua
         typescript
